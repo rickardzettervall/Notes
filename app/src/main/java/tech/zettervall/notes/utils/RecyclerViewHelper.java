1e7f88fdcef21f61bf5b6c0c400a725f9c0a240e
ZetFury <zetfury@protonmail.com>
2019-05-24 07:55:10 +0200

    add RecyclerView standard implementation methods

---
+++ app/src/main/java/tech/zettervall/notes/utils/RecyclerViewHelper.java
@@ -0,0 +1,28 @@
+package tech.zettervall.notes.utils;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class RecyclerViewHelper {
+
+    /**
+     * Get default LinearLayoutManager for RecyclerView.
+     */
+    public static LinearLayoutManager getDefaultLinearLayoutManager(Context context) {
+        return new LinearLayoutManager(context, RecyclerView.VERTICAL, false);
+    }
+
+    /**
+     * Set default RecyclerView decoration.
+     */
+    public static void setRecyclerViewDecoration(LinearLayoutManager linearLayoutManager,
+                                                 RecyclerView recyclerView) {
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recyclerView.getContext(),
+                        linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+}
