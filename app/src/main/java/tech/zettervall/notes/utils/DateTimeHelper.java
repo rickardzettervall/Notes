f149807552b8cc895d422c132d9f1ca658924ec7
ZetFury <zetfury@protonmail.com>
2019-06-07 16:04:05 +0200

    remake datestring to match country standards with am/pm and more

--- app/src/main/java/tech/zettervall/notes/utils/DateTimeHelper.java
+++ app/src/main/java/tech/zettervall/notes/utils/DateTimeHelper.java
@@ -1,12 +1,19 @@
 package tech.zettervall.notes.utils;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public abstract class DateTimeHelper {
+
+    private static final String TAG = DateTimeHelper.class.getSimpleName();
+    private static final Locale[] amPmCountries = { Locale.US, Locale.CANADA,
+            Locale.CANADA_FRENCH };
 
     /**
      * Get current Unix Epoch timestamp in long.
@@ -18,11 +25,46 @@ public abstract class DateTimeHelper {
     /**
      * Get Date String in local timezone from Epoch.
      */
+    // TODO: allow for user to change between am/pm and 24h
     public static String getDateStringFromEpoch(long epoch) {
-        Date date = new Date();
-        date.setTime(epoch);
-        DateFormat dateFormat = DateFormat.getDateTimeInstance();
-        dateFormat.setTimeZone(TimeZone.getDefault());
-        return dateFormat.format(date);
+        // Set Date Objects
+        Date currentDate = new Date();
+        currentDate.setTime(getCurrentEpoch());
+        Date inputDate = new Date();
+        inputDate.setTime(epoch);
+
+        // Convert current/input date to integers for comparison
+        SimpleDateFormat dayCheck = new SimpleDateFormat("dd", Locale.US),
+                yearCheck = new SimpleDateFormat("yyyy", Locale.US);
+        int currentDayVal = Integer.valueOf(dayCheck.format(currentDate)),
+                inputDayVal = Integer.valueOf(dayCheck.format(inputDate)),
+                currentYearVal = Integer.valueOf(yearCheck.format(currentDate)),
+                inputYearVal = Integer.valueOf(yearCheck.format(inputDate));
+
+        // Default HH/MM
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault()),
+                dateFormat = new SimpleDateFormat("d MMM", Locale.getDefault());
+
+        // Set AM/PM for countries which use that standard
+        for(int i = 0; i < amPmCountries.length; i++) {
+            if(Locale.getDefault().getDisplayCountry().equals(amPmCountries[i].getDisplayCountry())) {
+                timeFormat = new SimpleDateFormat("h:mm a", Locale.getDefault());
+                break;
+            }
+        }
+
+        if(currentDayVal - inputDayVal == 0) { // TODAY
+            return "Today, " + timeFormat.format(inputDate);
+        } else if(currentDayVal - inputDayVal == 1) { // YESTERDAY
+            return "Yesterday, " + timeFormat.format(inputDate);
+        } else if(currentYearVal == inputYearVal) { // THIS YEAR
+            return dateFormat.format(inputDate) + ", " + timeFormat.format(inputDate);
+        } else { // OTHER YEAR
+            dateFormat = new SimpleDateFormat("d MMM YYYY", Locale.getDefault());
+            return dateFormat.format(inputDate) + ", " + timeFormat.format(inputDate);
+//            DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+//            dateFormat.setTimeZone(TimeZone.getDefault());
+//            return dateFormat.format(inputDate) + ", " + timeFormat.format(inputDate);
+        }
     }
 }
