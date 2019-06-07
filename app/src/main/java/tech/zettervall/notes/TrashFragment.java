bcda923c8ddd8b7948026c03b68902436f57947b
ZetFury <zetfury@protonmail.com>
2019-06-07 16:05:37 +0200

    add trash fragment

---
+++ app/src/main/java/tech/zettervall/notes/TrashFragment.java
@@ -0,0 +1,21 @@
+package tech.zettervall.notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TrashFragment extends Fragment {
+
+    private static final String TAG = TrashFragment.class.getSimpleName();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
