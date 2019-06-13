cc4046df79714951073b24e946044f82f6989bd6
ZetFury <zetfury@protonmail.com>
2019-06-13 17:26:38 +0200

    add first version of sorting options

--- app/src/main/java/tech/zettervall/notes/NoteListFragment.java
+++ app/src/main/java/tech/zettervall/notes/NoteListFragment.java
@@ -1,16 +1,24 @@
 package tech.zettervall.notes;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -36,6 +44,7 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
     private NoteAdapter mNoteAdapter;
     private RecyclerView mRecyclerView;
     private FloatingActionButton mFab;
+    private SharedPreferences mSharedPreferences;
     private boolean mIsTablet;
 
     // Used for SearchView to restore state on configuration changes
@@ -50,6 +59,9 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
 
         // Initialize ViewModel
         mNoteListViewModel = ViewModelProviders.of(this).get(NoteListViewModel.class);
+
+        // Set SharedPreferences
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
         // Enable Toolbar MenuItem handling
         setHasOptionsMenu(true);
@@ -71,21 +83,17 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
         mRecyclerView.setAdapter(mNoteAdapter);
         mRecyclerView.setLayoutManager(mLayoutManager);
         RecyclerViewHelper.setRecyclerViewDecoration(mLayoutManager, mRecyclerView);
-        if (!mIsTablet) { // PHONE
-            mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-                @Override
-                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                    if (dy > 0) {
-                        mFab.hide();
-                    } else {
-                        mFab.show();
-                    }
-                    super.onScrolled(recyclerView, dx, dy);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) {
+                    mFab.hide();
+                } else {
+                    mFab.show();
                 }
-            });
-        } else { // TABLET
-            mFab.hide();
-        }
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
 
         // Set FAB OnClickListener
         mFab.setOnClickListener(new View.OnClickListener() {
@@ -111,6 +119,15 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
                 mNoteAdapter.submitList(notes);
             }
         });
+    }
+
+    /**
+     * Reload Observers, primarily for when user changes sorting.
+     */
+    private void refreshObservers() {
+        mNoteListViewModel.getNotes().removeObservers(getViewLifecycleOwner());
+        mNoteListViewModel.setNotes(null);
+        subscribeObservers();
     }
 
     /**
@@ -161,7 +178,7 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
                 mSearchQuery = query;
                 mSearchIconified = false;
                 mNoteListViewModel.getNotes().removeObservers(getViewLifecycleOwner());
-                mNoteListViewModel.setNotesSearch(query);
+                mNoteListViewModel.setNotes(query);
                 subscribeObservers();
             }
 
@@ -182,11 +199,8 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
         searchView.setOnCloseListener(new SearchView.OnCloseListener() {
             @Override
             public boolean onClose() {
-                mSearchIconified = true;
-                // Restore List to all Notes
-                mNoteListViewModel.getNotes().removeObservers(getViewLifecycleOwner());
-                mNoteListViewModel.setNotes();
-                subscribeObservers();
+                // Refresh List of Notes
+                refreshObservers();
                 return false;
             }
         });
@@ -196,7 +210,106 @@ public class NoteListFragment extends Fragment implements NoteAdapter.OnNoteClic
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_sort:
-                // TODO: sort
+
+                // Inflate View
+                View dialogView = View.inflate(getActivity(), R.layout.sort_alertdialog, null);
+
+                // Sort type (RadioGroup)
+                RadioGroup sortTypeGroup = dialogView.findViewById(R.id.sort_type_rg);
+                // Restore choice
+                int sortTypeChecked = mSharedPreferences.getInt(Constants.SORT_TYPE_KEY,
+                        Constants.SORT_TYPE_DEFAULT);
+                int checkedRadioButton = 0;
+                switch (sortTypeChecked) {
+                    case Constants.SORT_TYPE_ALPHABETICALLY:
+                        checkedRadioButton = R.id.sort_type_alphabetically_rb;
+                        break;
+                    case Constants.SORT_TYPE_CREATION_DATE:
+                        checkedRadioButton = R.id.sort_type_creation_date_rb;
+                        break;
+                    case Constants.SORT_TYPE_MODIFIED_DATE:
+                        checkedRadioButton = R.id.sort_type_modified_date_rb;
+                        break;
+                }
+                sortTypeGroup.check(checkedRadioButton);
+                // Set Listener
+                sortTypeGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        switch (checkedId) {
+                            case R.id.sort_type_alphabetically_rb: // Alphabetically
+                                mSharedPreferences.edit()
+                                        .putInt(Constants.SORT_TYPE_KEY,
+                                                Constants.SORT_TYPE_ALPHABETICALLY).apply();
+                                break;
+                            case R.id.sort_type_creation_date_rb: // Creation date
+                                mSharedPreferences.edit()
+                                        .putInt(Constants.SORT_TYPE_KEY,
+                                                Constants.SORT_TYPE_CREATION_DATE).apply();
+                                break;
+                            case R.id.sort_type_modified_date_rb: // Modified date
+                                mSharedPreferences.edit()
+                                        .putInt(Constants.SORT_TYPE_KEY,
+                                                Constants.SORT_TYPE_MODIFIED_DATE).apply();
+                                break;
+                        }
+
+                        // Refresh Date to show in list
+                        mNoteAdapter.setSortType();
+
+                        // Refresh List of Notes
+                        refreshObservers();
+                    }
+                });
+
+                // Sort with favorites on top (Checkbox)
+                CheckBox sortFavoritesOnTop = dialogView.findViewById(R.id.sort_favorites_ontop_cb);
+                boolean sortFavoritesBool = mSharedPreferences.getBoolean(
+                        Constants.SORT_FAVORITES_ON_TOP_KEY,
+                        Constants.SORT_FAVORITES_ON_TOP_DEFAULT);
+                sortFavoritesOnTop.setChecked(sortFavoritesBool);
+                sortFavoritesOnTop.setOnCheckedChangeListener(
+                        new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                mSharedPreferences.edit()
+                                        .putBoolean(Constants.SORT_FAVORITES_ON_TOP_KEY, isChecked).apply();
+
+                                // Refresh List of Notes
+                                refreshObservers();
+                            }
+                        });
+
+                DialogInterface.OnClickListener dialogClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case DialogInterface.BUTTON_POSITIVE: // Ascending
+                                        mSharedPreferences.edit()
+                                                .putInt(Constants.SORT_DIRECTION_KEY,
+                                                        Constants.SORT_DIRECTION_ASC).apply();
+                                        break;
+                                    case DialogInterface.BUTTON_NEGATIVE: // Descending
+                                        mSharedPreferences.edit()
+                                                .putInt(Constants.SORT_DIRECTION_KEY,
+                                                        Constants.SORT_DIRECTION_DESC).apply();
+                                        break;
+                                }
+
+                                // Refresh List of Notes
+                                refreshObservers();
+                            }
+                        };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(getString(R.string.sort_by));
+                builder.setView(dialogView);
+                builder.setPositiveButton(R.string.sort_by_ascending, dialogClickListener);
+                builder.setPositiveButtonIcon(getResources().getDrawable(R.drawable.ic_ascending));
+                builder.setNegativeButton(R.string.sort_by_descending, dialogClickListener);
+                builder.setNegativeButtonIcon(getResources().getDrawable(R.drawable.ic_descending));
+                builder.show();
                 break;
         }
         return false;
