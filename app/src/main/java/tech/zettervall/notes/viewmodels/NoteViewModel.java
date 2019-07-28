cb642b21d02ed1b720c1245577bf798743ac16d5
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-07-28 12:06:10 +0200

    update viewmodel to get raw list of tags

--- app/src/main/java/tech/zettervall/notes/viewmodels/NoteViewModel.java
+++ app/src/main/java/tech/zettervall/notes/viewmodels/NoteViewModel.java
@@ -5,8 +5,12 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import java.util.List;
+
 import tech.zettervall.notes.models.Note;
+import tech.zettervall.notes.models.Tag;
 import tech.zettervall.notes.repositories.NoteRepository;
+import tech.zettervall.notes.repositories.TagRepository;
 
 /**
  * ViewModel for a single Note.
@@ -15,10 +19,12 @@ import tech.zettervall.notes.repositories.NoteRepository;
 public class NoteViewModel extends AndroidViewModel {
 
     private NoteRepository mNoteRepository;
+    private TagRepository mTagRepository;
 
     public NoteViewModel(@NonNull Application application) {
         super(application);
         mNoteRepository = NoteRepository.getInstance(application);
+        mTagRepository = TagRepository.getInstance(application);
     }
 
     public long insertNote(Note note) {
@@ -31,5 +37,12 @@ public class NoteViewModel extends AndroidViewModel {
 
     public void deleteNote(Note note) {
         mNoteRepository.deleteNote(note);
+    }
+
+    /**
+     * Get Tags in List, used in AlertDialog for single Note.
+     */
+    public List<Tag> getTags() {
+        return mTagRepository.getTagsRaw();
     }
 }
