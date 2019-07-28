59beeb36f3bc304f1bac4a72545ae5f0185a2925
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-07-28 14:16:37 +0200

    add generic method for returning non-LiveData from db

---
+++ app/src/main/java/tech/zettervall/notes/utils/DbUtil.java
@@ -0,0 +1,40 @@
+package tech.zettervall.notes.utils;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public abstract class DbUtil {
+
+    /**
+     * Generic method for retrieving raw db objects (non-LiveData).
+     *
+     * @param callable Callable for retrieving db object
+     * @param <T>      Return type
+     * @return T
+     */
+    public static <T> T rawDB(Callable<T> callable) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<T> future = executorService.submit(callable);
+        T obj = null;
+        try {
+            obj = future.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Shutdown ExecutorService
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(500, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+
+        return obj;
+    }
+}
