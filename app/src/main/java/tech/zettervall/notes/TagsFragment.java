960c846e7f0e207940fa945bd2ea469b8f47029c
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-07-30 10:38:20 +0200

    add dialog for creating new Tag

--- app/src/main/java/tech/zettervall/notes/TagsFragment.java
+++ app/src/main/java/tech/zettervall/notes/TagsFragment.java
@@ -8,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,6 +27,7 @@ import java.util.List;
 import tech.zettervall.mNotes.R;
 import tech.zettervall.notes.adapters.NoteAdapter;
 import tech.zettervall.notes.adapters.TagAdapter;
+import tech.zettervall.notes.adapters.TagSelectAdapter;
 import tech.zettervall.notes.models.Note;
 import tech.zettervall.notes.models.Tag;
 import tech.zettervall.notes.utils.RecyclerViewUtil;
@@ -35,10 +37,7 @@ public class TagsFragment extends BaseListFragment implements TagAdapter.OnTagCl
 
     private static final String TAG = TagsFragment.class.getSimpleName();
     private TagListViewModel mTagListViewModel;
-    private LinearLayoutManager mLayoutManager;
     private TagAdapter mTagAdapter;
-    private RecyclerView mRecyclerView;
-    private FloatingActionButton mFab;
 
     @Nullable
     @Override
@@ -61,9 +60,50 @@ public class TagsFragment extends BaseListFragment implements TagAdapter.OnTagCl
         mRecyclerView.setAdapter(mTagAdapter);
         mRecyclerView.setLayoutManager(mLayoutManager);
         RecyclerViewUtil.setRecyclerViewDecoration(mLayoutManager, mRecyclerView);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) {
+                    mFab.hide();
+                } else {
+                    mFab.show();
+                }
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
 
-        // Hide FAB
-        mFab.hide();
+        // Set FAB OnClickListener
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inflate View
+                final View dialogView = View.inflate(getActivity(), R.layout.dialog_tag_new, null);
+
+                DialogInterface.OnClickListener dialogClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case DialogInterface.BUTTON_POSITIVE:
+                                        EditText newTag = dialogView.findViewById(R.id.new_tag_edittext);
+                                        String str = newTag.getText().toString()
+                                                .replaceAll("#", "");
+                                        mTagListViewModel.insertTag(new Tag(str));
+                                        break;
+                                    case DialogInterface.BUTTON_NEGATIVE:
+                                        break;
+                                }
+                            }
+                        };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(getString(R.string.action_tag_new));
+                builder.setView(dialogView);
+                builder.setPositiveButton(R.string.confirm_done, dialogClickListener);
+                builder.setNegativeButton(R.string.abort, dialogClickListener);
+                builder.show();
+            }
+        });
 
         // Set title
         getActivity().setTitle(getString(R.string.action_tags));
@@ -95,7 +135,6 @@ public class TagsFragment extends BaseListFragment implements TagAdapter.OnTagCl
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_trash, menu);
     }
 
     @Override
