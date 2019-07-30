8d1431b9902425562aedcd0f55422dadd56332e2
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-07-30 11:14:20 +0200

    add documentation for method

--- app/src/main/java/tech/zettervall/notes/utils/KeyboardUtil.java
+++ app/src/main/java/tech/zettervall/notes/utils/KeyboardUtil.java
@@ -5,6 +5,9 @@ import android.view.WindowManager;
 
 public abstract class KeyboardUtil {
 
+    /**
+     * Hides keyboard from activity.
+     */
     public static void hideKeyboard(Activity activity) {
         activity.getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
