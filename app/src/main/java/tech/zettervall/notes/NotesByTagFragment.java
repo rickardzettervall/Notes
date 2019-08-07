cd5ccb1a6a81af23dd0f4256f68aadfcc7cd95da
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-08-07 13:56:15 +0200

    add NotesByTagFragment and associated ViewModel

---
+++ app/src/main/java/tech/zettervall/notes/NotesByTagFragment.java
@@ -0,0 +1,106 @@
+package tech.zettervall.notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.RecyclerView;
+
+import tech.zettervall.mNotes.R;
+import tech.zettervall.notes.adapters.NoteAdapter;
+import tech.zettervall.notes.models.Note;
+import tech.zettervall.notes.utils.RecyclerViewUtil;
+import tech.zettervall.notes.viewmodels.NotesByTagFragmentViewModel;
+
+public class NotesByTagFragment extends BaseListFragment {
+
+    private static final String TAG = AllNotesFragment.class.getSimpleName();
+    private NotesByTagFragmentViewModel mNotesByTagFragmentViewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView = inflater.inflate(R.layout.fragment_notelist, container, false);
+
+        // Initialize ViewModel
+        mNotesByTagFragmentViewModel = ViewModelProviders.of(this).get(NotesByTagFragmentViewModel.class);
+
+        // Find Views
+        mRecyclerView = rootView.findViewById(R.id.fragment_notelist_recyclerview);
+        mFab = rootView.findViewById(R.id.fragment_notelist_fab);
+        emptyTextView = rootView.findViewById(R.id.fragment_notelist_is_empty_textview);
+
+        // Set Adapter / LayoutManager / Decoration
+        mNoteAdapter = new NoteAdapter(this);
+        mLayoutManager = RecyclerViewUtil.getDefaultLinearLayoutManager(getActivity());
+        mRecyclerView.setAdapter(mNoteAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        RecyclerViewUtil.setRecyclerViewDecoration(mLayoutManager, mRecyclerView);
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0) {
+                    mFab.hide();
+                } else {
+                    mFab.show();
+                }
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+        // Set FAB OnClickListener
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callback.onFragmentFabClick(false);
+            }
+        });
+
+        // Set title
+        getActivity().setTitle(R.string.action_all_notes);
+
+        // Subscribe Observers
+        subscribeObservers();
+
+        return rootView;
+    }
+
+    @Override
+    public void subscribeObservers() {
+        mNotesByTagFragmentViewModel.getNotes().observe(this, new Observer<PagedList<Note>>() {
+            @Override
+            public void onChanged(PagedList<Note> notes) {
+                mNoteAdapter.submitList(notes);
+                emptyTextView.setVisibility(notes.isEmpty() ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public void refreshObservers(@Nullable String query) {
+        mNotesByTagFragmentViewModel.getNotes().removeObservers(getViewLifecycleOwner());
+        mNotesByTagFragmentViewModel.setNotes(query);
+        subscribeObservers();
+    }
+
+    @Override
+    public void onNoteClick(int index) {
+        try {
+            // Get Note
+            Note note = mNoteAdapter.getCurrentList().get(index);
+            // Send Note to callback interface
+            callback.onNoteClick(note);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/tech/zettervall/notes/viewmodels/NotesByTagFragmentViewModel.java
@@ -0,0 +1,38 @@
+package tech.zettervall.notes.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+
+import tech.zettervall.notes.Constants;
+import tech.zettervall.notes.models.Note;
+import tech.zettervall.notes.repositories.NoteRepository;
+
+/**
+ * ViewModel for NotesByTagFragment.
+ */
+public class NotesByTagFragmentViewModel extends AndroidViewModel {
+
+    private NoteRepository mNoteRepository;
+    private LiveData<PagedList<Note>> mNotes;
+
+    public NotesByTagFragmentViewModel(@NonNull Application application) {
+        super(application);
+        mNoteRepository = NoteRepository.getInstance(application);
+        setNotes(null);
+    }
+
+    public LiveData<PagedList<Note>> getNotes() {
+        return mNotes;
+    }
+
+    public void setNotes(@Nullable String query) {
+        mNotes = new LivePagedListBuilder<>(mNoteRepository.getNotesPagedList(query),
+                Constants.NOTE_LIST_PAGE_SIZE).build();
+    }
+}
