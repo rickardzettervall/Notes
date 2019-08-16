07a1048998df130ed6157714a77d9fe663df32c7
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-08-16 12:10:19 +0200

    set correct naming and add documentation

--- app/src/test/java/tech/zettervall/notes/utils/StringUtilTest.java
+++ app/src/test/java/tech/zettervall/notes/utils/StringUtilTest.java
@@ -6,8 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class StringUtilTest {
 
+    /**
+     * Checks that String gets returned with first character upper cased.
+     */
     @Test
-    public void isFirstCharUpperCased_returnTrue() {
+    public void setFirstCharUpperCase() {
         assertEquals("Hello", StringUtil.setFirstCharUpperCase("hello"));
     }
 }
