93136bb386079ade29d29f9320e2ce5db8921c97
Rickard Zettervall <rickard.zettervall@protonmail.com>
2019-08-28 10:26:54 +0200

    add method to show keyboard

--- app/src/main/java/tech/zettervall/notes/utils/KeyboardUtil.java
+++ app/src/main/java/tech/zettervall/notes/utils/KeyboardUtil.java
@@ -1,16 +1,28 @@
 package tech.zettervall.notes.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 
 public abstract class KeyboardUtil {
 
     /**
-     * Hides keyboard from activity.
+     * Hide Keyboard.
      */
     public static void hideKeyboard(Activity activity) {
         activity.getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
         );
+    }
+
+    /**
+     * Show Keyboard.
+     */
+    public static void showKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager)
+                activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(),
+                InputMethodManager.SHOW_FORCED, 0);
     }
 }
