b9730ec570be7c04ad35371e8f5b2946edc81d41
Rickard Zettervall <rickard.zettervall@protonmail.com>
2020-03-04 09:53:43 +0100

    test: Update UI test for creating new Note

--- app/src/androidTest/java/tech/zettervall/notes/ui/CreateNewNoteTest.java
+++ app/src/androidTest/java/tech/zettervall/notes/ui/CreateNewNoteTest.java
@@ -1,19 +1,11 @@
 package tech.zettervall.notes.ui;
 
 import android.content.pm.ActivityInfo;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -23,7 +15,7 @@ import org.junit.runner.RunWith;
 import tech.zettervall.mNotes.R;
 import tech.zettervall.notes.MainActivity;
 import tech.zettervall.notes.NoteActivity;
-import tech.zettervall.notes.repositories.NoteRepository;
+import tech.zettervall.notes.TestHelper;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -31,11 +23,10 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
+import static tech.zettervall.notes.RecyclerViewAssertions.itemViewMatches;
 
 @RunWith(AndroidJUnit4.class)
 public class CreateNewNoteTest {
@@ -44,35 +35,17 @@ public class CreateNewNoteTest {
     public ActivityTestRule<MainActivity> mMainActivity = new ActivityTestRule<>(MainActivity.class);
     private boolean isTablet;
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
     @Before
     public void init() {
-        isTablet = mMainActivity.getActivity().getResources().getBoolean(R.bool.isTablet);
         Intents.init();
+        isTablet = mMainActivity.getActivity().getResources().getBoolean(R.bool.isTablet);
+        TestHelper.ClearDb(mMainActivity.getActivity());
+        TestHelper.sleepThread(250);
     }
 
     @After
     public void release() {
-        // Delete all Notes from db
-        NoteRepository.getInstance(mMainActivity.getActivity().getApplication()).deleteAllNotes();
+        TestHelper.ClearDb(mMainActivity.getActivity());
     }
 
     @Test
@@ -98,27 +71,15 @@ public class CreateNewNoteTest {
         onView(withId(R.id.fragment_note_title_edittext)).check(matches(withText(title)));
         onView(withId(R.id.fragment_note_text_edittext)).check(matches(withText(text)));
 
-        // Go back
-        ViewInteraction appCompatImageButton = onView(
-                allOf(withContentDescription("Navigate up"),
-                        childAtPosition(
-                                allOf(withId(R.id.toolbar),
-                                        childAtPosition(
-                                                withId(R.id.app_bar_layout),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatImageButton.perform(click());
+        if (!isTablet) { // Go back
+            onView(withContentDescription("Navigate up")).perform(click());
+        } else { // Save
+            onView(withId(R.id.fragment_note_fab)).perform(click());
+        }
 
-        // Check that Note was inserted into db
-        ViewInteraction linearLayout = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.fragment_notelist_recyclerview),
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.RelativeLayout.class),
-                                        0)),
-                        0),
-                        isDisplayed()));
-        linearLayout.check(matches(isDisplayed()));
+        // Check that Note was inserted into db and is now displayed in adapter
+        onView(withId(R.id.fragment_notelist_recyclerview))
+                .check(itemViewMatches(0, R.id.list_note_title_textview, withText(title)))
+                .check(itemViewMatches(0, R.id.list_note_text_textview, withText(text)));
     }
 }
