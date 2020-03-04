40b9b8d46b727eadb8f5f590edeb387c00156f72
Rickard Zettervall <rickard.zettervall@protonmail.com>
2020-03-04 10:53:24 +0100

    test: Add UI test for searching Notes

---
+++ app/src/androidTest/java/tech/zettervall/notes/ui/NoteListSearchTest.java
@@ -0,0 +1,62 @@
+package tech.zettervall.notes.ui;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import tech.zettervall.mNotes.R;
+import tech.zettervall.notes.MainActivity;
+import tech.zettervall.notes.TestHelper;
+import tech.zettervall.notes.repositories.NoteRepository;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static tech.zettervall.notes.RecyclerViewAssertions.itemCountMatches;
+import static tech.zettervall.notes.RecyclerViewAssertions.itemViewMatches;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteListSearchTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private NoteRepository mNoteRepository;
+
+    @Before
+    public void init() {
+        mNoteRepository = NoteRepository.getInstance(mActivityTestRule.getActivity().getApplication());
+        mNoteRepository.deleteAllNotes();
+        mNoteRepository.insertNotes(TestHelper.getMockNotes());
+        TestHelper.sleepThread(250);
+    }
+
+    @After
+    public void release() {
+        mNoteRepository.deleteAllNotes();
+    }
+
+    /**
+     * Sort Notes.
+     */
+    @Test
+    public void search() {
+        onView(withId(R.id.action_search)).perform(click());
+        onView(withId(R.id.search_src_text)).perform(replaceText("beta"));
+
+        // Check that only Note Beta is displayed
+        onView(withId(R.id.fragment_notelist_recyclerview))
+                .check(itemCountMatches(1));
+        onView(withId(R.id.fragment_notelist_recyclerview))
+                .check(itemViewMatches(
+                        0,
+                        R.id.list_note_title_textview,
+                        withText(TestHelper.NOTE_BETA_TITLE)));
+    }
+}
