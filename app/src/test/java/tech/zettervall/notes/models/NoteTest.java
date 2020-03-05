96bfb677ee0ec8bd1d151910a34d1aba66e4fc41
Rickard Zettervall <rickard.zettervall@protonmail.com>
2020-03-05 12:46:16 +0100

    test: Cleanup

--- app/src/test/java/tech/zettervall/notes/models/NoteTest.java
+++ app/src/test/java/tech/zettervall/notes/models/NoteTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertNotEquals;
 public class NoteTest {
 
     private String title1, title2, text1, text2;
-    private long creationEpoch, modifiedEpoch_1, modifiedEpoch_2, notificationEpoch;
+    private long creationEpoch, modifiedEpoch, notificationEpoch;
     private ArrayList<Integer> tagIDs;
 
     /**
@@ -26,8 +26,7 @@ public class NoteTest {
         text1 = "this is the text";
         text2 = "This is the text";
         creationEpoch = DateTimeUtil.getCurrentEpoch();
-        modifiedEpoch_1 = DateTimeUtil.getCurrentEpoch();
-        modifiedEpoch_2 = DateTimeUtil.getCurrentEpoch() + 1000L;
+        modifiedEpoch = DateTimeUtil.getCurrentEpoch();
         notificationEpoch = -1;
         tagIDs = new ArrayList<>();
     }
@@ -38,7 +37,7 @@ public class NoteTest {
     @Test
     public void isTitleAndText_upperCased_returnTrue() {
         Note note = new Note(1, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_1, notificationEpoch, false, false);
+                modifiedEpoch, notificationEpoch, false, false);
 
         assertEquals(note.getTitle(), title2);
         assertEquals(note.getText(), text2);
@@ -51,26 +50,12 @@ public class NoteTest {
     @Test
     public void isNotesEqual_identical_returnTrue() {
         Note note1 = new Note(1, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_1, notificationEpoch, false, false);
+                modifiedEpoch, notificationEpoch, false, false);
         Note note2 = new Note(1, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_1, notificationEpoch, false, false);
+                modifiedEpoch, notificationEpoch, false, false);
 
         assertEquals(note1, note2);
         System.out.println("The Notes are equal!");
-    }
-
-    /**
-     * Compare two Notes which represents the same Note but which has been updated.
-     */
-    @Test
-    public void isNotesEqual_updated_returnFalse() {
-        Note note1 = new Note(1, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_1, notificationEpoch, false, false);
-        Note note2 = new Note(1, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_2, notificationEpoch, false, false);
-
-        assertNotEquals(note1, note2);
-        System.out.println("The Notes are not equal!");
     }
 
     /**
@@ -79,9 +64,9 @@ public class NoteTest {
     @Test
     public void isNotesEqual_differentIds_returnFalse() {
         Note note1 = new Note(1, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_1, notificationEpoch, false, false);
+                modifiedEpoch, notificationEpoch, false, false);
         Note note2 = new Note(2, title1, text1, null, tagIDs, creationEpoch,
-                modifiedEpoch_1, notificationEpoch, false, false);
+                modifiedEpoch, notificationEpoch, false, false);
 
         assertNotEquals(note1, note2);
         System.out.println("The Notes are not equal!");
