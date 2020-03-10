a4a6ac41f1c65418687250d29a4af9d4e7e2c654
Rickard Zettervall <rickard.zettervall@protonmail.com>
2020-03-10 17:18:24 +0100

    style: Minor reformat

--- app/src/main/java/tech/zettervall/notes/SettingsActivity.java
+++ app/src/main/java/tech/zettervall/notes/SettingsActivity.java
@@ -137,8 +137,7 @@ public class SettingsActivity extends AppCompatActivity {
                         .setMessage(getString(R.string.confirm_restore_db_message))
                         .show();
             } else if (preference == findPreference(getString(R.string.about_other_apps_key))) { // View other Apps
-                Uri devPages = Uri.parse(Constants.GOOGLE_PLAY_STORE);
-                Intent intent = new Intent(Intent.ACTION_VIEW, devPages);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.GOOGLE_PLAY_STORE));
                 intent.setPackage("com.android.vending");
                 if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
                     startActivity(intent);
